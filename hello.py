e2e752dc25f45ace5614fd51013af67a7ef42ee7
FaseehBajwa298 <faseehbajwa786@gmail.com>
2025-10-20 10:02:39 -0400

    feat: hello.py supports optional name arg and updated message

--- hello.py
+++ hello.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
+import sys
+
 
 def main():
-    print("Hello from DevOps-CI-Demo2!")
+    name = sys.argv[1] if len(sys.argv) > 1 else "DevOps-CI-Demo2"
+    print(f"Hello, {name}! Feature update is live.")
+
 
 if __name__ == "__main__":
     main()
